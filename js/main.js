dd9df7c1db679f395747b9b78819373fdd974535
Yaser Castillo <yacer120@gmail.com>
2014-09-02 20:05:54 -0600

    Using Handlebar Templates

--- js/main.js
+++ js/main.js
@@ -1,17 +1,12 @@
 var app = {
-
-    findByName: function() {
-        console.log('findByName');
-        this.store.findByName($('.search-key').val(), function(employees) {
-            var l = employees.length;
-            var e;
-            $('.employee-list').empty();
-            for (var i=0; i<l; i++) {
-                e = employees[i];
-                $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
-            }
-        });
-    },
+//Find employes by Name
+findByName: function() {
+    var self = this;
+    this.store.findByName($('.search-key').val(), function(employees) {
+        $('.employee-list').html(self.employeeLiTpl(employees));
+    });
+},
+    //Just for sample of showing alert
     showAlert: function (message, title) {
     if (navigator.notification) {
         navigator.notification.alert(message, null, title, 'OK');
@@ -19,15 +14,21 @@ var app = {
         alert(title ? (title + ": " + message) : message);
     }
 },
-
+ 
+ //Used to render the view to hTML Code
+    renderHomeView: function() {
+    $('body').html(this.homeTpl());
+    $('.search-key').on('keyup', $.proxy(this.findByName, this));
+}, 
+//Initialize the renderings
 initialize: function() {
     var self = this;
+    this.homeTpl = Handlebars.compile($("#home-tpl").html());
+    this.employeeLiTpl = Handlebars.compile($("#employee-li-tpl").html());
     this.store = new MemoryStore(function() {
-        self.showAlert('Store Initialized', 'Info');
+        self.renderHomeView();
     });
-    $('.search-key').on('keyup', $.proxy(this.findByName, this));
 }
 };
-
-
+//ru
 app.initialize();
